53388ebd9d92986e6321a49831fa6917d102dae1
Igor <imartynkin@yandex.ru>
2022-10-24 00:55:31 +0900

    Refactor code ТЗ16

--- gateway/src/main/java/ru/practicum/shareit/item/GatewayItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/GatewayItemController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.exception.Create;
-import ru.practicum.shareit.exception.NullDataException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
